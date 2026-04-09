77c867e8a9c754e9e104ad07d6452229f42652ab
Botmaster55 <wajsarif461@gmail.com>
2026-04-09 22:46:32 +0530

    Update info.py

--- info.py
+++ info.py
@@ -79,7 +79,7 @@ QR_CODE_IMAGE = environ.get("QR_CODE_IMAGE", "https://image.zaw-myo.workers.dev/
 # =========================================================
 START_PIC = environ.get("START_PIC", "https://image.zaw-myo.workers.dev/image/89652e23-b14f-4aaa-b295-e9f5c93ee3b7")
 AUTH_PICS = environ.get("AUTH_PICS", "")
-VERIFY_IMG = environ.get("VERIFY_IMG", "")
+VERIFY_IMG = environ.get("VERIFY_IMG", "https://image.zaw-myo.workers.dev/image/13e5c437-0197-4239-b39f-895674d035fc")
 NO_IMG = environ.get("NO_IMG", "")
 
 # =========================================================
