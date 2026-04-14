f69081259f0fb37cc6a29d1f83ece9cab39c2cd6
Botmaster55 <wajsarif461@gmail.com>
2026-04-14 10:44:22 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -178,3 +178,13 @@ async def auto_delete_message(message, dlt_msg):
         await message.delete()
     except:
         pass
+# ---------------- RANDOM NAME ---------------- #
+import random
+import string
+
+def generate_weird_name(length=8):
+    return ''.join(random.choices(string.ascii_letters + string.digits, k=length))
+
+# ---------------- THUMBNAIL ---------------- #
+async def generate_thumbnail(file):
+    return None
